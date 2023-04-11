35761d1db739235fa5a4a5ae59dfb483e8944f51
HosseinKarimi128 <Hossein.karimi.0128@gmail.com>
2023-04-11 17:35:32 +0330

    1

--- contlika/settings.py
+++ contlika/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
            'NAME': 'imagica-dev',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
-           'HOST': '0.0.0.0',
+           'HOST': 'db',
            'PORT': 5432
        }
    }
