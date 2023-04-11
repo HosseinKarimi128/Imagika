e090af336132a5be013865a8049892221e70faa6
HosseinKarimi128 <Hossein.karimi.0128@gmail.com>
2023-04-12 01:08:58 +0330

    add migrate to last docker-compose

--- contlika/settings.py
+++ contlika/settings.py
@@ -73,10 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'contlika.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
