e6a99ac9163e7340635593839fa1efe233f2df48
HosseinKarimi128 <Hossein.karimi.0128@gmail.com>
2023-04-11 18:04:52 +0330

    1

--- Dockerfile
+++ Dockerfile
@@ -7,17 +7,38 @@
 # ENV DJANGO_SETTINGS_MODULE=contlika.settings
 # ENV PYTHONUNBUFFERED=1
 # EXPOSE 8000
-FROM python:3.9
+# FROM python:3.9
 
+# ENV PYTHONUNBUFFERED 1
+
+# RUN mkdir /code
+# WORKDIR /code
+
+# COPY requirements.txt /code/
+# RUN pip install -r requirements.txt
+
+# COPY . /code/
+
+# EXPOSE 8000
+# EXPOSE 8001
+# pull official base image
+FROM python:3.9.6-alpine
+
+# set work directory
+WORKDIR /usr/src/app
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN mkdir /code
-WORKDIR /code
+# install psycopg2 dependencies
+RUN apk update \
+    && apk add postgresql-dev gcc python3-dev musl-dev
 
-COPY requirements.txt /code/
+# install dependencies
+RUN pip install --upgrade pip
+COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY . /code/
-
-EXPOSE 8000
-EXPOSE 8001
+# copy project
+COPY . .
--- contlika/settings.py
+++ contlika/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'contlika.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'imagica-dev',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '0.0.0.0',
-           'PORT': 5432
-       }
-   }
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
 
 LOGGING = {
     'version': 1,
